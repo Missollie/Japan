759c6b6d567f8c526d1d52a2b522bad1fb8e04f6
OLha Kuzmenchuk <olhakuzmenchuk@gmail.com>
2023-11-10 15:34:26 +0200

    csroll

--- src/js/app.js
+++ src/js/app.js
@@ -2,4 +2,6 @@ import * as Functions from "./modules/functions.js";
 Functions.footer();
 import * as Burger from "./modules/burger.js";
 Burger.burger();
+import * as Scroll from "./modules/scroll.js";
+Scroll.scroll();
 
